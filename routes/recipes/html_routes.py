5b20521c308c8cca02a6b502257eaae440155dc7
Chirag Givan <chirag.givan@gmail.com>
2026-02-25 21:37:00 +0000

    edited api docs.

--- routes/recipes/html_routes.py
+++ routes/recipes/html_routes.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, session, abort
-#from flask_jwt_extended import jwt_required, get_jwt_identity
 from db import get_db_connection
 from mysql.connector import Error
 from config import bootstrap
-# from routes.recipes.api_routes.read_recipes import get_recipe_details
 
 recipes_html_bp = Blueprint("recipes_html", __name__, url_prefix="/recipes")
 
